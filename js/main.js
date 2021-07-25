7dadcf3fc0518429251148b98245356598ac1973
Anastasia Kuryleva <knastya76@yandex.ru>
2021-07-25 15:27:16 +0300

    Created: media and js for modal

--- js/main.js
+++ js/main.js
@@ -14,25 +14,32 @@ var modalButton = $(".booking__button");
 var closeModalButton = $(".modal__close");
 
 modalButton.on("click", function() {
-  console.log('клик');
   openModal();
 });
 closeModalButton.on("click", function() {
   closeModal();
 });
 
+
 function openModal() {
   var modalOverlay = $(".modal__overlay");
   var modalDialog = $(".modal__dialog");
-  modalOverlay.addClass("modal__overlay_visible")
-  modalDialog.addClass("modal__dialog_visible")
+  modalOverlay.addClass("modal__overlay_visible");
+  modalDialog.addClass("modal__dialog_visible");
+  $(document).on('keydown', function(e){
+      if(e.which === 27) {
+      closeModal();
+    }
+  }); 
 }
 function closeModal() {
   var modalOverlay = $(".modal__overlay");
   var modalDialog = $(".modal__dialog");
-  modalOverlay.removeClass("modal__overlay_visible")
-  modalDialog.removeClass("modal__dialog_visible")
+  modalOverlay.removeClass("modal__overlay_visible");
+  modalDialog.removeClass("modal__dialog_visible");
 }
+
+
 
 var hotelSwiper = new Swiper('.hotel-slider__container', {
   
